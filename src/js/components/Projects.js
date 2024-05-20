f48e2c4f9ca91bbb61cbcbf483a1a949c4bf403c
Stephen Tyler <stephenjovon@gmail.com>
2024-05-20 05:06:27 -0400

    Added 'Find Happy Hour' project to projects

--- src/js/components/Projects.js
+++ src/js/components/Projects.js
@@ -1,19 +1,23 @@
 import React from "react";
 const projects = [
   {
-    title: "Stephen Tyler Dev",
+    title: "Find happy hour",
     description:
-      "This was a project I built to showcase my growth in web development. I honed in on my basic javascript and React skills, emphasising E6 best practices, and I focused on data flow in ReactJS.",
-    image: require("../../img/stephen_dev_project.png"),
+      "Find Happy Hour is a project I developed to assist travelers and locals find food and drink specials in different locales.",
+    image: require("../../img/3-devices-black.png"),
     tech: [
       { name: "HTML5", img: require("../../img/HTML5_Badge.png") },
       { name: "CSS3", img: require("../../img/CSS3_Badge.png") },
       { name: "Javascript", img: require("../../img/Javascript_Badge.png") },
       { name: "React", img: require("../../img/ReactJS_Badge.png") },
+      { name: "Node", img: require("../../img/NodeJS_Badge.png") },
+      { name: "Express", img: require("../../img/ExpressJS_Badge.png") },
+      { name: "Mongoose", img: require("../../img/Mongoose_Badge.png") },
     ],
-    url: "https://leggosteveo.github.io/stephen-tyler-dev/",
-    gitHub: "https://github.com/leggosteveo/stephen-tyler-dev",
+    url: "https://findhappyhour.onrender.com",
+    gitHub: "https://github.com/leggosteveo/find-happy-hour",
   },
+
   {
     title: "Luther Construction",
     description:
@@ -30,6 +34,20 @@ const projects = [
     ],
     url: "https://frontend-1cc6.onrender.com",
     gitHub: "https://github.com/leggosteveo/luther-construction",
+  },
+  {
+    title: "Stephen Tyler Dev",
+    description:
+      "This was a project I built to showcase my growth in web development. I honed in on my basic javascript and React skills, emphasising E6 best practices, and I focused on data flow in ReactJS.",
+    image: require("../../img/stephen_dev_project.png"),
+    tech: [
+      { name: "HTML5", img: require("../../img/HTML5_Badge.png") },
+      { name: "CSS3", img: require("../../img/CSS3_Badge.png") },
+      { name: "Javascript", img: require("../../img/Javascript_Badge.png") },
+      { name: "React", img: require("../../img/ReactJS_Badge.png") },
+    ],
+    url: "https://leggosteveo.github.io/stephen-tyler-dev/",
+    gitHub: "https://github.com/leggosteveo/stephen-tyler-dev",
   },
   {
     title: "Diana Carlstrom Events",
@@ -51,13 +69,11 @@ const Project = ({ project }) => {
       <p>{project.description}</p>
       <div>
         {project.image && (
-          <img src={project.image} className="img-fluid" alt=""/>
+          <img src={project.image} className="img-fluid" alt="" />
         )}
       </div>
       <div>
-        {project.tech.length > 1 && (
-          <h4 className="tech-used">Tech Used:</h4>
-        )}
+        {project.tech.length > 1 && <h4 className="tech-used">Tech Used:</h4>}
         {project.tech.map((tech) => (
           <img src={tech.img} alt={tech.name} className="tech-used-badge" />
         ))}
@@ -77,7 +93,6 @@ const Project = ({ project }) => {
     </div>
   );
 };
-
 
 const Projects = () => (
   <section id="projects-section" className="container inner px-3 px-lg-5">
